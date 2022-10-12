b54d285bbf97ae3433bf7625e021de8593781cc9
Ozay Akcan <mail@ozayakcan.com.tr>
2022-10-12 22:09:04 +0300

    Değişkenlere decimal eklendi.

--- lib/veritabani/degiskenler.dart
+++ lib/veritabani/degiskenler.dart
@@ -1,9 +1,19 @@
+import 'package:decimal/decimal.dart';
+
 class Degiskenler {
   static int parseInt({
     required String? sayi,
     int varsayilan = 0,
   }) {
     return int.tryParse(sayi ?? "0") ?? varsayilan;
+  }
+
+  static Decimal parseDecimal({
+    required String? sayi,
+    Decimal? varsayilan,
+  }) {
+    varsayilan = varsayilan ?? Decimal.parse("0.00");
+    return Decimal.tryParse(sayi ?? "0.00") ?? varsayilan;
   }
 
   static bool parseBool({
