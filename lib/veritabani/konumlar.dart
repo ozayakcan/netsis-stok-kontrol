37f76c39d84b20902727298202dbd120d115576e
Ozay Akcan <mail@ozayakcan.com.tr>
2022-10-21 12:04:18 +0300

    Web versiyonu etkinleştirildi. Api düzenlendi.

--- lib/veritabani/konumlar.dart
+++ lib/veritabani/konumlar.dart
@@ -9,6 +9,6 @@ class Konumlar {
   }
   String get url => "http://$host:8080";
 
-  String get stoklar => "$url/app/stoklar";
-  String get stokHareketleri => "$url/app/stokHareket";
+  String get stoklar => "$url/api/stoklar";
+  String get stokHareketleri => "$stoklar/hareketler";
 }
